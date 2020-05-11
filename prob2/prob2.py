7e019a966d1967863026c71c247774bcb8f15552
cpsc455 <wolfken81@gmail.com>
2020-05-10 21:03:28 -0700

    I finished doing the adding of products to the dataframe Now to do calculations on them

--- prob2/prob2.py
+++ prob2/prob2.py
@@ -28,6 +28,40 @@ with open('customer.txt','r') as f:
                     df.loc[linenumber,temp] = word2
         linenumber = linenumber + 1
 
+with open('product.txt','r') as f:
+    #This is to get rid of the newline character
+    mylist = f.read().splitlines() 
+    linenumber = 0
+    CID = None
+    lastprod = None
+    for line in mylist:
+        for word in line.split(';'):
+            print(word)
+            temp = None
+            x = 0
+            for word2 in word.split(':'):
+                print(word2)
+                #this skips the first label
+                if x == 0:
+                    temp = word2
+                    temp = str(temp)
+                    x = x + 1
+                elif x == 1:
+                    print("Going into switch temp is "+temp+"123")
+                    print(type(temp))
+                    if temp == 'CUSTOMER ID ':
+                        print("Going into CUSTOMER ID")
+                        CID = word2
+                    elif temp == ' PRODUCT NAME ':
+                        print("Going into PRODUCT NAME")
+                        rowIndex = df[df['CUSTOMER ID ']==CID].index[0]
+                        df.loc[rowIndex,word2] = '0'
+                        lastprod = word2
+                    elif temp == ' QUANTITY ':
+                        print("Going into QUANTITY")
+                        rowIndex = df[df['CUSTOMER ID ']==CID].index[0]
+                        df.loc[rowIndex,lastprod] = word2
+        linenumber = linenumber + 1
 
 print(df)
 
